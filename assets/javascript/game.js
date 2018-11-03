eace0cf21c43f9dcbddf10c77e2db7ebdc9c331d
Chillen <josh.jenkin@live.com>
2018-11-03 15:36:58 -0600

    first tries, html/css in

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var monsters = ["Dracula, Frankenstein, Wolfman, Mummy, Godzilla"];
+
+var wins = 0;
+
+
+var wins = document.getElementById("wins-text");
+var currentWord = document.getElementById("current-word");
+var guessMade = document.getElementById("num-guesses");
+var lettersGuessed = document.getElementById("letters-guessed");
+
+document.onkeyup = function(event); {
+
+    var userGuess = event.key;
+
+    
+}
+
+
+
+
