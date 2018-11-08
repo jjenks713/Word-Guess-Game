231c0f71f370ebb6c64fa50e5c26d01f49a0da9f
Chillen <josh.jenkin@live.com>
2018-11-07 21:07:57 -0700

    got id's to work, need to get _ _ _ and correct letters to show

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,105 @@
- //pick a random word
-    var monsterWords = ["Dracula", "Frankenstein", "Wolfman", "Mummy", "Godzilla"];
-    var lettersInWord = [];
-    var numberOfBlanks = 0;
-    var selectedWord = "";
-    // keep track of score
-    var wins = 0;
-    //keep track of guesses left
-    var guessLeft = 10;
-    // while (numGuess = 0) {
-    //     numGuesses.textContent = "Game Over!"
-    // }
+//pick a random word
+var monsterWords = ["Dracula", "Frankenstein", "Wolfman", "Mummy", "Godzilla"];
+var lettersInWord = [];
+var numberOfBlanks = 0;
+var selectedWord = "";
 
-    function getRandomMonsters() {
-        selectedWord = monsterWords[Math.floor(Math.random() = monsters.length)];
-        console.log(selectedWord);
-    }
+// keep track of score
+var wins = 0;
 
-    function calculateLettersInWord() {
-        numbersOfBlanks = monsterWords.length;
-        console.log(numbersOfBlanks);
-    }
-    function getLettersInWOrd() {
-        lettersInWord = selectedWord.split('')
-    }
+//keep track of guesses left
+var guessLeft = 10;
+
+// grab id's from HTML
+
+var winsText = document.getElementById("wins-text");
+var wordLines = document.getElementById("word-lines");
+var numGuesses = document.getElementById("num-guesses");
+var lettersGuessed = document.getElementById("letters-guessed");
 
 
+winsText.textContent = wins;
+numGuesses.textContent = guessLeft;
 
+function getRandomMonsters() {
+    selectedWord = monsterWords[Math.floor(Math.random() * monsterWords.length)];
+    console.log(selectedWord);
+}
 
-    //display guessed letters
+function calculateLettersInWord() {
+    numberOfBlanks = selectedWord.length;
+    console.log(numberOfBlanks);
+}
+function getLettersInWOrd() {
+    lettersInWord = selectedWord.split('');
+    for (var i = 0; i < selectedWord.split('').length; i++) {
+        i = "_";
+        wordLines.textContent = selectedWord.split('')[i];
+        }
 
-
-    var winsText = document.getElementById("wins-text");
-    var wordLines = document.getElementById("word-lines");
-    var numGuesses = document.getElementById("num-guesses");
-    var lettersGuessed = document.getElementById("letters-guessed");
-
-
-
-    // function grabsWord(word) {
-
-
-
-    // userGuess function:
-    document.onkeyup = function (event) {
-        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
+    console.log(lettersInWord);
+}
+document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase(); 
         
+        if (userGuess === lettersInWord) {
+            wordLines.textContent = selectedWord.split('');
             
+        } else {
+            guessLeft--;
+            lettersGuessed.textContent = userGuess;
+        }   
         
-            if (userGuess === monsterWords[0].charAt[1]) {
-                wordLines.textContent = userGuess;
-            }
-            if (userGuess === "r") /*monsters[0].charAt[1])*/ {
-                wordLines.textContent = userGuess;
-            } 
-            if (userGuess === "a") /*monsters[0].charAt[2])*/ {
-                wordLines.textContent = userGuess;
-            }
-            if (userGuess === "c") /*monsters[0].charAt[3])*/ {
-                wordLines.textContent = userGuess;
-            }
-            if (userGuess === "u") /*monsters[0].charAt[4])*/ {
-                wordLines.textContent = userGuess;
-            }
-            if (userGuess === "l") /*monsters[0].charAt[5])*/ {
-                wordLines.textContent = userGuess;
-            }
-            else if (userGuess === monsters[0]) {
-                    wins++;
-                
-            } else {
-                lettersGuessed.textContent = userGuess;
-                guessLeft--;
-            }
+        // alert("youclicked");
+        
+    
+
+
+winsText.textContent = wins;
+numGuesses.textContent = guessLeft;
+};
+function leftOver() {
+    if (guessLeft <= 1) {
+        numGuesses.textContent = "Game Over!";  
+    } 
+    
+};
+
+leftOver()
+getRandomMonsters();
+calculateLettersInWord();
+getLettersInWOrd();
 
 
 
-        winsText.textContent = wins;
-        // wordLines.textContent = ;
-        numGuesses.textContent = guessLeft;
 
 
-    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
